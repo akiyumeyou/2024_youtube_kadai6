0c8ff56c6f7471330ef932d0742b80fbc5c985ee
akiyumeyou <160195773+akiyumeyou@users.noreply.github.com>
2024-03-20 12:03:50 +0900

    Add files via upload

---
+++ js/firebaseK.js
@@ -0,0 +1,9 @@
+export default  {
+    apiKey: "AIzaSyBFQexauk43k3yH5TZGW6F7WeohcWKH1vU",
+    authDomain: "potz-line.firebaseapp.com",
+    databaseURL: "https://potz-line-default-rtdb.firebaseio.com",
+    projectId: "potz-line",
+    storageBucket: "potz-line.appspot.com",
+    messagingSenderId: "662095335717",
+    appId: "1:662095335717:web:005f6ac6496b0ee5774acc"
+  };
---
+++ js/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+   "./index.html",
+   '.js/*.{js,ts,jsz,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
